396489f55123a12bc502f8ef21a5398d770f3850
nuelxlencedjango <nuelxlencedjango@gmail.com>
2020-10-15 12:00:14 +0530

    web scraping

--- webscript/settings.py
+++ webscript/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os ,django_heroku
+import os 
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
